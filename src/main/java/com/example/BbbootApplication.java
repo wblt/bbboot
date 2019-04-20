89a49ffa6d251d2b79a90089dd696a1c628849fc
wblt <940422068@qq.com>
2019-04-20 18:57:09 +0800

    d

--- src/main/java/com/example/BbbootApplication.java
+++ src/main/java/com/example/BbbootApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
@@ -11,6 +12,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.example.mapper")
 //扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
 @ComponentScan(basePackages= {"com.example", "org.n3r.idworker"})
+//开启定时任务
+@EnableScheduling
 public class BbbootApplication {
 
     public static void main(String[] args) {
